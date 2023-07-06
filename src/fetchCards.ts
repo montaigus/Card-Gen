e04ab7363ca10cc2a5587f04607f4dc21339c882
Montaigus <montaigus@hotmail.com>
2023-07-06 19:36:18 +0200

    ajout de cors et typescript, nodemon ignore cards

--- src/fetchCards.ts
+++ src/fetchCards.ts
@@ -1,11 +1,10 @@
 const fetchCards = async ({ queryKey }) => {
   const id = queryKey[1];
-  const apiRes = await fetch("./Cards.json");
+  const apiRes = await fetch("http://localhost:8000/cards");
 
   if (!apiRes.ok) {
     throw new console.error("not ok");
   }
-
   return apiRes.json();
 };
 
