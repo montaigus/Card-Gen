60f7913a2f43a7407470cf5d5ab1edc6e9bbea4c
Xavier <xavier.cazalot@gmail.com>
2023-07-10 15:41:42 +0200

    Use the query result instead of an empty state

--- src/AllCards.jsx
+++ src/AllCards.jsx
@@ -2,21 +2,15 @@ import { useQuery } from "@tanstack/react-query";
 import ExistingCard from "./ExistingCard";
 import CardLayout from "./CardLayout";
 import { getCards } from "./api";
-import { useState } from "react";
 
 const AllCards = () => {
   //aller chercher les données sur le server
   const result = useQuery(["cards", 0], getCards);
 
-  //tentative pour mettre toutes les cartes dans un state pour le rafraichir dès qu'il y a un
-  //! card est undefined lors de l'instanciation du composant
-  const [cards, setCards] = useState([]);
-
   return (
     <CardLayout title="Voir les cartes existantes" class="existingCards">
-      {/* cards est undefined, pourquoi ? */}
       {!result.isLoading &&
-        cards.map((cardItem) => (
+        result.data.map((cardItem) => (
           <ExistingCard key={cardItem.id} cardItem={cardItem} />
         ))}
     </CardLayout>
