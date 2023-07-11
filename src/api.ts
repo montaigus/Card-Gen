b9d036af866c0abb3c771f1d6cf4842def9ec6eb
Montaigus <montaigus@hotmail.com>
2023-07-11 12:08:13 +0200

    Refactor api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,21 @@
 // TODO Refactor similar code/config into reusable functions
 
+const requestOptionsPost = (bodyObject) => {
+  return {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify(bodyObject, null, 2),
+  };
+};
+
+const localhost = "http://localhost:8000";
+
 export async function getCards({ queryKey }) {
   const id = queryKey[1];
-  const apiRes = await fetch("http://localhost:8000/cards");
+  const apiRes = await fetch(localhost + "/cards");
 
   if (!apiRes.ok) {
     throw new console.error("not ok");
@@ -12,28 +25,12 @@ export async function getCards({ queryKey }) {
 }
 
 export async function writeCard({ cardItem, isExisting }) {
-  const requestOptions = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-    },
-    body: JSON.stringify(cardItem, null, 2),
-  };
   const apiRes = fetch(
-    `http://localhost:8000/write?new=${isExisting ? 0 : 1}`,
-    requestOptions
+    `${localhost}/write?new=${isExisting ? 0 : 1}`,
+    requestOptionsPost(cardItem)
   );
 }
 
 export async function destroyCard(id) {
-  const requestOptions = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-    },
-    body: JSON.stringify({ id: id }, null, 2),
-  };
-  const apiRes = fetch("http://localhost:8000/destroy", requestOptions);
+  const apiRes = fetch(localhost + "/destroy", requestOptionsPost({ id: id }));
 }
